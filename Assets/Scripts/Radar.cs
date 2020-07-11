3574d18202c296d56de851f3d9e44a3b3c6f3941
Pedro Balaguer <pedro.balaguer@hotmail.com>
2020-07-10 23:04:06 -0300

    Add RayCast from player to targets

---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    public GameObject[] targets;
+
+    private void FixedUpdate()
+    {
+        int layerMask = 0;
+
+
+        //Debug.DrawRay(transform.position, target.transform.position - transform.position);
+        foreach (GameObject target in targets)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, target.transform.position - transform.position);
+            if (hit != null)
+            {
+                if (Vector2.Distance(transform.position, target.transform.position) > hit.distance)
+                {
+                    Debug.DrawLine(hit.point, transform.position, Color.green);
+                }
+            }
+        }
+    }
+}
