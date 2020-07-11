d16229a7f7c41b9709474ae673bececea999ca72
Pedro Balaguer <pedro.balaguer@hotmail.com>
2020-07-10 23:23:45 -0300

    Show icons in radar

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -6,20 +6,38 @@ public class Radar : MonoBehaviour
 {
     public GameObject[] targets;
 
+    private List<GameObject> targetIcons = new List<GameObject>();
+
     private void FixedUpdate()
     {
-        int layerMask = 0;
-
+        // Remove previous target icons
+        foreach (GameObject targetIcon in targetIcons)
+        {
+            Destroy(targetIcon);
+        }
+        targetIcons.Clear();
 
         //Debug.DrawRay(transform.position, target.transform.position - transform.position);
         foreach (GameObject target in targets)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, target.transform.position - transform.position);
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
             if (hit != null)
             {
                 if (Vector2.Distance(transform.position, target.transform.position) > hit.distance)
                 {
                     Debug.DrawLine(hit.point, transform.position, Color.green);
+                    SpriteRenderer spriteRenderer;
+                    if (target.TryGetComponent(out spriteRenderer))
+                    {
+                        GameObject targetIcon = new GameObject("TargetIcon");
+                        targetIcon.transform.position = hit.point - direction * spriteRenderer.size;
+                        SpriteRenderer renderer = targetIcon.AddComponent<SpriteRenderer>();
+                        renderer.sprite = spriteRenderer.sprite;
+
+                        targetIcons.Add(targetIcon);
+                    }
                 }
             }
         }
