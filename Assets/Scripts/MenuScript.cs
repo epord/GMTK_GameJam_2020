36fa9c804810582f1a3292e50715e1defee6c06c
Diego de Rochebouet <diego.rochebouet@gmail.com>
2020-07-12 03:12:15 -0300

    Add menus

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
