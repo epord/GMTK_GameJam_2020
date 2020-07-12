e4d70c0ba93850cfaa7ed52553577617bf6cd412
Pedro Balaguer <pedro.balaguer@hotmail.com>
2020-07-12 03:48:03 -0300

    Die in the sun

---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision with sun");
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<ErrorActivator>().Lose();
+        }
+    }
+}
